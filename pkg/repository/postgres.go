d13527694a6b8acd82e2a6683749d12aab34ff5e
Dell <maria.miroshnychenko@gmail.com>
2024-05-19 19:28:05 +0300

    test

---
+++ pkg/repository/postgres.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+/*func NewPostgresDB(cfg Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBname, cfg.SSLMode,
+	)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}*/
---
+++ schhema/000001_init.up.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users
+(
+    id    serial unique not null ,
+    name  varchar(255) not null,
+    username varchar(255) unique not null ,
+    password_hash varchar(255) not null
+);
+CREATE TABLE todo_lists
+(
+    id serial unique not null ,
+    title varchar(255) not null,
+    description varchar(255)
+);
+CREATE TABLE users_lists
+(
+    id serial PRIMARY KEY ,
+    user_id int references users(id) on delete cascade not null,
+    list_id int references todo_lists(id) on delete cascade not null
+);
+CREATE TABLE todo_items
+(
+    id serial PRIMARY KEY ,
+    title varchar(255) not null ,
+    description varchar(255),
+    done boolean not null default false
+);
+CREATE TABLE lists_items
+(
+    id serial PRIMARY KEY ,
+    item_id int references todo_items(id) on delete cascade not null,
+    list_id int references todo_lists(id) on delete cascade not null
+);
